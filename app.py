12526940250b88a0b3222c7a229379a8f723407d
Farbod <farbod.ab@hotmail.ca>
2018-12-26 18:03:29 -0500

    Set up basic app directories

---
+++ app.py
@@ -0,0 +1,4 @@
+from cardiacpedia import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ cardiacpedia/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+
+##########################################################
+##################CONFIGURATIONS##########################
+##########################################################
+
+app.config['SECRET_KEY'] = '\x0c\xf7\x91\xab\xd0\x12\x90\xb7\x7fp\x18rqh\xefF'
+
+
+##########################################################
+##################DATABASE SETUPS#########################
+##########################################################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+##########################################################
+##################LOGIN CONFIGS#########################
+##########################################################
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "users.login"
+
+##########################################################
+##################Blueprint configs#########################
+##########################################################
