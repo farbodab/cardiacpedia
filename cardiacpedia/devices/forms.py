49a1b72498dc96c2ca9cf8e9aa43221f07d08736
Farbod <farbod.ab@hotmail.ca>
2018-12-30 14:50:07 -0500

    Search functionality working

---
+++ cardiacpedia/devices/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_login import current_user
+from cardiacpedia.models import IPG
+
+
+class Find_IPG(FlaskForm):
+    manufacturer = StringField('Manufacturer')
+    model_number = StringField('Model Number')
+    name = StringField('Name')
+    submit = SubmitField('Find Device')
--- cardiacpedia/devices/views.py
+++ cardiacpedia/devices/views.py
@@ -1,17 +1,33 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint, render_template_string
 from cardiacpedia import db
-from cardiacpedia.models import user_manager
-from cardiacpedia.models import IPG
+from cardiacpedia.models import user_manager,IPG
+from cardiacpedia.devices.forms import Find_IPG
 from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
+
 
 devices = Blueprint('devices', __name__)
 
 ################################################################################
 ##############################View Table########################################
 ################################################################################
-@devices.route('/devices/ipg')
+@devices.route('/devices/ipg', methods=['GET', 'POST'])
 @login_required
 def ipg():
+    form = Find_IPG()
+    if form.validate_on_submit():
+        page = request.args.get('page', 1, type=int)
+        devices = IPG.query
+        FILTERS = [form.manufacturer.data, form.model_number.data, form.name.data]
+        NAMES = [IPG.manufacturer, IPG.model_number, IPG.name]
+        if form.manufacturer.data:
+            devices = devices.filter(IPG.manufacturer.like('%' + form.manufacturer.data + '%'))
+        if form.model_number.data:
+            devices = devices.filter(IPG.model_number.like('%' + form.model_number.data + '%' ))
+        if form.name.data:
+            devices = devices.filter(IPG.name.like('%' + form.name.data + '%' ))
+        devices = devices.paginate(page=page, per_page=10)
+        return render_template('ipg.html', devices=devices, page_title='IPG Low-Voltage Devices', form=form)
+
     page = request.args.get('page', 1, type=int)
     devices = IPG.query.paginate(page=page, per_page=10)
-    return render_template('ipg.html', devices=devices, page_title='IPG Low-Voltage Devices')
+    return render_template('ipg.html', devices=devices, page_title='IPG Low-Voltage Devices', form=form)
