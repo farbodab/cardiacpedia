5637b5694582fc0427d1cda762926efe1ae2afa2
Farbod <farbod.ab@hotmail.ca>
2018-12-30 20:41:09 -0500

    Removed about us page

--- cardiacpedia/core/views.py
+++ cardiacpedia/core/views.py
@@ -6,8 +6,3 @@ core = Blueprint('core', __name__)
 def index():
     #home page view
     return render_template('index.html', page_title="Home")
-
-@core.route('/about')
-def about():
-    #about page
-    return render_template('about.html', page_title="About Us")
