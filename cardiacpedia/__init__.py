db7c2d166a136b91332c501b1a15fdbdeda21b0d
Farbod <farbod.ab@hotmail.ca>
2019-01-04 19:12:04 -0500

    Error handling

--- cardiacpedia/__init__.py
+++ cardiacpedia/__init__.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
 from flask_bootstrap import Bootstrap
 from config import Config
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -35,3 +37,16 @@ app.register_blueprint(core)
 app.register_blueprint(error_pages)
 app.register_blueprint(users)
 app.register_blueprint(devices)
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('CardiacBook startup')
